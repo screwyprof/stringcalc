03c882776454b3f9ff5dbcc854785b697460a5a1
Max O'Donnell <screwyprof@gmail.com>
2019-03-11 14:56:52 +0300

    Acceptance test added

---
+++ cmd/stringcal_test.go
@@ -0,0 +1,18 @@
+package main
+
+func Example() {
+	run()
+
+	// Output:
+	// 0
+	// 5
+	// 7
+	// 7
+	// 6
+	// An error occurred: invalid input given: non-numbers
+	// 3
+	// An error occurred: negative numbers not allowed: -2,-4
+	// 2
+	// 6
+	// 6
+}
--- cmd/stringcalc.go
+++ cmd/stringcalc.go
@@ -2,23 +2,28 @@ package main
 
 import (
 	"fmt"
-	"os"
-
 	"github.com/screwyprof/stringcalc"
 )
 
 func main() {
-	calc := stringcalc.StringCalc{}
-
-	sum, err := calc.Add("//[***]\n1***2***3")
-	failOnError(err)
-
-	fmt.Println(sum)
+	run()
 }
 
-func failOnError(err error) {
-	if err != nil {
-		fmt.Printf("An error occurred: %v", err)
-		os.Exit(1)
+func run() {
+	calc := stringcalc.StringCalc{}
+
+	data := []string{"", "5", "5,2", "3,2,1,0,1", "1\n2,3", "non-numbers",
+		"//;\n1;2", "1,-2,-4", "2,1001", "//[***]\n1***2***3", "//[*][%]\n1*2%3"}
+	for _, input := range data {
+		addAndPrintSum(calc, input)
 	}
+}
+
+func addAndPrintSum(calc stringcalc.StringCalc, input string) {
+	sum, err := calc.Add(input)
+	if err != nil {
+		fmt.Printf("An error occurred: %v\n", err)
+		return
+	}
+	fmt.Println(sum)
 }
